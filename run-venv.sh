af22d58b05543a915787741c682719db3a30694b
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-08-25 16:42:30 +0300

    Removed langsmith config

--- run-venv.sh
+++ run-venv.sh
@@ -3,7 +3,7 @@ source myenv/bin/activate
 python3 -m pip install --upgrade pip
 export SYSTEM_VERSION_COMPAT=1
 pip3 install -r requirements.txt
-streamlit run app_url.py
+streamlit run app.py
 # python3 back_sql.py
 # deactivate
 # rm -rf myenv
