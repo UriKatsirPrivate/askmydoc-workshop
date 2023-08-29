867601a4d0ce5f6ef020f72de597363882f68f2a
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-08-29 15:29:01 +0300

    Code refactor

--- app_text.py
+++ app_text.py
@@ -1,36 +1,42 @@
 import streamlit as st
-from back import *
+from back import generate_response_from_llm
 
-# Page title
+# Use constants for option keys
+URL_OPTION = 'URL'
+FILE_UPLOAD_OPTION = 'File Upload'
+
+# Define allowed file types as a constant
+ALLOWED_FILE_TYPES = {'txt', 'py', 'tf', 'sh', 'yaml'}
+
+# Use a dictionary for option mapping
+option_mapping = {
+    'Please Select': None,
+    FILE_UPLOAD_OPTION: FILE_UPLOAD_OPTION,  # Simplify the mapping
+    # Add more options here
+}
+
 st.set_page_config(page_title='Ask My Doc App')
 st.title('Ask My Doc App')
 
-options = ['Please Select','File Upload']
-selected_option = st.selectbox('Select Document Type', options)
+# Use more descriptive variable names
+selected_option_label = st.selectbox('Select Document Type', list(option_mapping.keys()))
+selected_option_value = option_mapping[selected_option_label]
 
-url_text = None
 uploaded_file = None
-query_text = None
 
-if selected_option == 'URL':
-    # Do nothing for now
-    a=1
-elif selected_option == 'File Upload':
-    uploaded_file = st.file_uploader('Upload an article', type=['txt', 'py', 'tf', 'sh', 'yaml'])
-    query_text = st.text_input('Enter your question:', placeholder='Please provide a short summary.')
+if selected_option_value == FILE_UPLOAD_OPTION:
+    uploaded_file = st.file_uploader('Upload an article', type=ALLOWED_FILE_TYPES)
+    query_text = st.text_input('Enter your question:', value='', help='Please provide a short summary.')
 
-# Form input and query
 with st.form('myform', clear_on_submit=True):
-    submitted = st.form_submit_button('Submit', disabled=not (uploaded_file or url_text))
+    submitted = st.form_submit_button('Submit', disabled=not uploaded_file)
     if submitted:
         try:
-            with st.spinner('Thinking...'):
-                if selected_option == 'URL':
-                    # Do nothing for now
-                    a=1
-                else:
+            if uploaded_file:
+                with st.spinner('Generating response...'):
                     response = generate_response_from_llm(uploaded_file, query_text)
-            st.write(response)
-            
+                st.write(response)
+            else:
+                st.warning("Please upload a valid file.")  # Use warning for non-blocking notification
         except Exception as e:
-            st.error(f"Error during query generation or processing: {str(e)}")
+            st.error(f"An error occurred: {str(e)}")
