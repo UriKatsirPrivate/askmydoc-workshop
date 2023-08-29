6064246faa4f62ab66a9bd818ef1149c9e3053e2
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-08-29 15:13:08 +0300

    Code refactor

--- app_basic.py
+++ app_basic.py
@@ -1,19 +1,37 @@
 import streamlit as st
 
-# Page title
+# Constants
+ACCEPTED_FILE_TYPES = ['txt', 'py', 'tf', 'sh', 'yaml']
+DOCUMENT_OPTIONS = ['Please Select', 'URL', 'File Upload']
+
+# Function to get user input based on selected option
+def get_user_input(selected_option):
+    query_text = st.text_input('Enter your question:', placeholder='Please provide a short summary.')
+
+    if selected_option == 'URL':
+        url_text = st.text_input('Enter your URL:', placeholder='Please provide a URL.')
+        return url_text, query_text
+    elif selected_option == 'File Upload':
+        uploaded_file = st.file_uploader('Upload an article', type=ACCEPTED_FILE_TYPES)
+        return uploaded_file, query_text
+    else:
+        return None, query_text
+
+# Page setup
 st.set_page_config(page_title='Ask My Doc App')
 st.title('Ask My Doc App')
 
-options = ['Please Select', 'URL', 'File Upload']
-selected_option = st.selectbox('Select Document Type', options)
+# Document type selection
+selected_option = st.selectbox('Select Document Type', DOCUMENT_OPTIONS)
 
-url_text = None
-uploaded_file = None
-query_text = None
-# temp_file_path = ""
-if selected_option == 'URL':
-    url_text = st.text_input('Enter your url:', placeholder='Please provide a URL.')
-    query_text = st.text_input('Enter your question:', placeholder='Please provide a short summary.')
-elif selected_option == 'File Upload':
-    uploaded_file = st.file_uploader('Upload an article', type=['txt', 'py', 'tf', 'sh', 'yaml'])
-    query_text = st.text_input('Enter your question:', placeholder='Please provide a short summary.')
+# Get user input
+document_data, query_text = get_user_input(selected_option)
+
+# Process user input
+if document_data is not None:
+    # Process the document data (URL or uploaded file)
+    st.write(f"Document data: {document_data}")
+
+# Process the user's query
+if query_text:
+    st.write(f"User's query: {query_text}")
