33b40a0dceec140c8b8d5a11c6a49b20aa2991b2
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-09-06 21:36:04 +0300

    if running from Cloud Shell

--- run-venv.sh
+++ run-venv.sh
@@ -3,6 +3,7 @@ source myenv/bin/activate
 python3 -m pip install --upgrade pip
 export SYSTEM_VERSION_COMPAT=1
 pip3 install -r requirements.txt
-streamlit run app_basic.py
+streamlit run app_url.py
+# streamlit run app.py --server.port 8081 --browser.serverAddress 0.0.0.0 --server.enableCORS False
 # deactivate
 # rm -rf myenv
