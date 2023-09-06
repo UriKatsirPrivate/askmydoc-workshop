cd05d63a34aeffbdf2e170d798a074f79945985e
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-09-06 22:29:11 +0300

    running from CloudShell

--- back.py
+++ back.py
@@ -3,6 +3,11 @@ from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.embeddings import VertexAIEmbeddings
 from initialization import *
+# Add the 3 lines below only if running from CloudShell
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# ======================================================
 
 
 llm = initialize_llm()
