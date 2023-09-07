b32f0801f4906373a5f56af954f744cad3f5d573
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-09-07 10:31:15 +0300

    Changed to port 8080

--- run-venv.sh
+++ run-venv.sh
@@ -4,6 +4,6 @@ python3 -m pip install --upgrade pip
 export SYSTEM_VERSION_COMPAT=1
 pip3 install -r requirements.txt
 streamlit run app_url.py
-# streamlit run app.py --server.port 8081 --browser.serverAddress 0.0.0.0 --server.enableCORS False
+# streamlit run app.py --server.port 8080 --browser.serverAddress 0.0.0.0 --server.enableCORS False
 # deactivate
 # rm -rf myenv
