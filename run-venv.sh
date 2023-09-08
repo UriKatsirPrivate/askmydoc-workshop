d6c65aab98445ac787fba86fb77d05f48ac17efc
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-09-08 17:29:38 +0300

    good for both local and cloud shell execution

--- run-venv.sh
+++ run-venv.sh
@@ -3,7 +3,8 @@ source myenv/bin/activate
 python3 -m pip install --upgrade pip
 export SYSTEM_VERSION_COMPAT=1
 pip3 install -r requirements.txt
-streamlit run app_url.py
-# streamlit run app.py --server.port 8080 --browser.serverAddress 0.0.0.0 --server.enableCORS False
+# streamlit run app_text.py
+# This one is good for both local and cloud shell execution
+streamlit run app.py --server.port 8080 --browser.serverAddress 0.0.0.0 --server.enableCORS False
 # deactivate
 # rm -rf myenv
